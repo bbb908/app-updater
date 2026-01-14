16fd95744fe5b72261e89f9365267dc174b0eeb9
bbb908 <105934310+bbb908@users.noreply.github.com>
2026-01-14 20:42:51 +0000

    Update autoUPG.bat

--- autoUPG.bat
+++ autoUPG.bat
@@ -1,14 +1,8 @@
-::Hello file editor changing any of this code may break it!
 @echo off
 
-
-:SOP
-set AppCreator=bbb908
-goto E1E!2£sE
 :continue
 cls
 echo ---App Upgrader---
-echo made by: %AppCreator%
 echo.
 echo 1. upgrade specified programme
 echo 2. upgrade ALL apps
@@ -25,89 +19,3 @@ echo.
 set/p "programme=Enter programme name: "
 winget upgrade "%programme%"
 goto continue
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-:E1E!2£sE
-if not %AppCreator%==bbb908 goto failed
-goto continue
-
-:failed
-echo OOPS!
-echo sorry but we couldn't verify this was a real version
-echo of this programme.
-echo.
-echo EXTRA_INFO: App_Edit_NUL
-pause
-exit
